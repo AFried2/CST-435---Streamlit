947f0183ad59340ffe14669f36875ae621b69227
AFried2 <alexfried@live.com>
2025-09-12 21:15:02 -0700

    updated app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import pandas as pd
 st.title('CST-435 Streamlit') 
 st.text('On a scale from 1-10 how much are you paying attentiion to Artzi?')
 st.slider('Attention Level:', 1, 10, 5)
+st.text('What is your favorite programming language?')
+st.selectbox('Programming Language:', ['Python', 'JavaScript', 'Java', 'C++', 'Other'])
+st.text('What is your favorite web framework?')
+st.selectbox('Web Framework:', ['Streamlit', 'Django', 'Flask', 'React', 'Other'])
